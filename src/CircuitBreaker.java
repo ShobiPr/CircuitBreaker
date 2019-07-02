f2d0586de8d73ac50e2ca969e0f56dda60dc517a
ShobiPr <shobiha.premkumar@stud.ntnu.no>
2019-07-02 15:46:44 +0200

    Started on Circuit Breajer

--- src/CircuitBreaker.java
+++ src/CircuitBreaker.java
@@ -1,2 +1,56 @@
 public class CircuitBreaker {
+
+    int[] errorCounter;
+    long[] failureTime;
+    boolean[] states;
+    long timeout = 2000*10000;
+
+
+    public CircuitBreaker(int numServer){
+        this.state = new boolean[numServer];
+        this.errorCounter = new int[numServer];
+        this.lastFailureTime = new long[numServer];
+    }
+
+    // true - good, false - broken
+    // 1 = new check, 2 - set in queue, 0 - call port
+   public int getPort(Message msg) {
+       int res = 0;
+       if (!states[port]) {
+           if (System.nanoTime() - failureTime[port] > timeout) {
+               res = 1; // call port again
+           } else {
+               res = 2; // set in queue
+           }
+       }
+       return res;
+   }
+
+    public Message handleRespond(int port, int status, Message msg) {
+        switch (status) {
+            case -1: //timeout
+
+
+                break;
+
+            case 0: // OK
+                return null;
+
+            case 1: //fail
+                errorCounter[port]++;
+                failureTime[port] = System.nanoTime();
+                states[port] = false;
+                //log
+                //send msg to queue (main)
+                break;
+
+            default:
+                System.out.println("Unexpected behaviour");
+        }
+
+        return msg;
+
+    }
+
 }
+
