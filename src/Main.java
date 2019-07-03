e2dfbe2542c61988c058f7a7fd714775587175cc
Mvrcus97 <marcus-tierney@hotmail.com>
2019-07-03 12:56:14 +0200

    Beginning on main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args){
 
-        //COMMENICATION AREA
+        //COMMUNICATION AREA
+        int MAX_MESSAGES = 1000; // set appropriate max size
+        int AMOUNT_MESSAGES = 100;
+
+        //Create messages
+        ArrayList<Message> messageList = new ArrayList<Message>();
+        Message msg;
+        int randomPort;
+        for(int i = 0; i < AMOUNT_MESSAGES; i ++){
+            randomPort = (int)(Math.random() * 100);
+            msg = new Message(i,"This is message number: " +i, randomPort );
+            messageList.add(msg);
+        }
+
 
         //send messages from deliveryService to CB
+
+
+
 
         //call on server with given portNum from CB
 
